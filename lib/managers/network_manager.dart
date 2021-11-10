3462687afe466d21fdeec39b41791e48e41ba3dd
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-11-09 22:07:31 -0300

    feat: created NetworkManager and fetchEvents method

---
+++ lib/managers/network_manager.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:event_tracker_pm26s/models/event.dart';
+
+import 'package:http/http.dart';
+
+class NetworkManager {
+  NetworkManager._init();
+  static final NetworkManager shared = NetworkManager._init();
+
+  final _baseURL = 'https://app.ticketmaster.com/discovery/v2';
+
+  Future<List<Event>> fetchEventsNearCalgary() async {
+    final endPoint = _baseURL +
+        '/events.json?city=Calgary&apikey=79X4shnyf8bicpEGm6YsWrG1nVeEJXQR';
+    Response response = await get(Uri.parse(endPoint));
+
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body)["_embedded"]["events"];
+
+      List<Event> events = body
+          .map(
+            (dynamic item) => Event.fromJson(item),
+          )
+          .toList();
+
+      return events;
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+}
