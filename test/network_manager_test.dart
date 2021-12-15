9c18bcbb7c91ad7b255f417e6d6aec3370f71394
italobampi <86384289+italobampi@users.noreply.github.com>
2021-12-15 16:18:00 -0300

    testes utils e network

--- test/network_manager_test.dart
+++ test/network_manager_test.dart
@@ -3,9 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   final network = NetworkManager.shared;
-  test("deve retornar uma lista de eventos ", () async {
+  test("fetchEventsNearby: deve retornar uma lista de eventos ", () async {
     final list = await network.fetchEventsNearby(34.0522342, -118.2436849);
     expect(list.isNotEmpty, equals(true));
     expect(list.first.name!.isNotEmpty, equals(true));
   });
+
+
+  test("fetchEventsBySearch: deve retornar uma lista de eventos  comp palavras chave", () async {
+    final list = await network.fetchEventsBySearch("foo");
+    print(list.first.name);
+    expect(list.isNotEmpty, equals(true));
+  });
+
+
 }
---
+++ test/utils/date_utils_test.dart
@@ -0,0 +1,16 @@
+import 'package:event_tracker_pm26s/utils/date_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  test("deve retornar 12/06/2021 06:59 AM", (){
+    print(convertDate('2021-12-06T06:59:00'));
+    expect(convertDate('2021-12-06T06:59:00Z'), equals("12/06/2021 06:59 AM"));
+  });
+
+  test("a data imconpleta deve gerar erro", (){
+
+    expect(()=> convertDate('2021-12-06T06:59'),
+        throwsA(TypeMatcher<Exception>()));
+  });
+
+}
